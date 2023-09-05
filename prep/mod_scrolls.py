687ef59ea51e257725413b8abbd67686fd4f88ea
darkcharl <darkcharl@void.com>
2023-09-05 18:00:33 -0400

    feat: make platform agnostic

--- prep/mod_scrolls.py
+++ prep/mod_scrolls.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 
+import os
 import re
 from pathlib import Path
 
 enabled_spells = []
 mod_sources = [
- #"../../../Source/Shared_PAK/Public/Shared/RootTemplates",
- #"../../../Source/Gustav_PAK/Public/Gustav/RootTemplates",
- #"../../../Source/Gustav_PAK/Public/GustavDev/RootTemplates",
- "../../secret-scrolls/PAK/Public/SecretScrolls/RootTemplates",
- "../../omniscient-wizards/PAK/Public/OmniscientWizards/RootTemplates",
+    "../../../Source/Shared_PAK/Public/Shared/RootTemplates",
+    "../../../Source/Gustav_PAK/Public/Gustav/RootTemplates",
+    "../../../Source/Gustav_PAK/Public/GustavDev/RootTemplates",
+    "../../SecretScrolls/PAK/Public/SecretScrolls/RootTemplates",
+    "../../OmniscientWizards/PAK/Public/OmniscientWizards/RootTemplates",
 ]
+
 
 def load_enabled_spells(enabled_file="enabled_spells.txt"):
     enabled_spells = {}
@@ -36,13 +38,14 @@ def find_files(spells_re, src='.'):
 
 def load_spells_re():
     enabled_spells = load_enabled_spells()
-    match_re = re.compile(f'id="(S[kp][ei]llI[dD])" type="FixedString" value="({"|".join(enabled_spells)})"')
+    match_re = re.compile(
+        f'id="(S[kp][ei]llI[dD])" type="FixedString" value="({"|".join(enabled_spells)})"')
     repl_re = r'id="\1" type="FixedString" value="\2_Original"'
     return match_re, repl_re
-    
+
 
 def get_dst_path(file):
-    mod_dir = str(file).split('/')[-3]
+    mod_dir = str(file).split(os.sep)[-3]
     return Path(f'modded_scrolls/{mod_dir}/{file.name}')
 
 
@@ -55,9 +58,14 @@ def modify_object(src_file, dst_file, match_re, repl_re):
 if __name__ == "__main__":
     match_re, repl_re = load_spells_re()
     for mod in mod_sources:
-        for src in find_files(src=mod, spells_re=match_re):
+        for src in Path(mod).iterdir():
+            if not src.is_file():
+                continue
+            with src.open(encoding='latin1') as f:
+                if not re.search(match_re, f.read()):
+                    # print(f' [.] skipping {src}')
+                    continue
             print(f' [<] {src}')
             dst = get_dst_path(src)
-            modify_object(src, get_dst_path(src), match_re, repl_re)
+            modify_object(src, dst, match_re, repl_re)
             print(f' [>] {dst}\n')
-            
